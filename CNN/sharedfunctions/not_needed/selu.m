5b943c324f690e2c160606d8e1aa6ff37fe69247
roboball <ronsummers44@gmail.com>
2017-06-22 15:09:00 +0200

    Update selu.m

--- CNN/sharedfunctions/not_needed/selu.m
+++ CNN/sharedfunctions/not_needed/selu.m
@@ -1,5 +1,5 @@
 function [ out ] = selu( x )
-         %%% selu activation function %%%
+         %% selu activation function %%
 
          % init globals
          ALPHA = 1.6732632423543772848170429916717;
