4e7a9065e75959aaa748a45a63137f2748eef64b
roboball <ronsummers44@gmail.com>
2017-06-22 15:08:03 +0200

    Add files via upload

---
+++ CNN/sharedfunctions/not_needed/selu.m
@@ -0,0 +1,15 @@
+function [ out ] = selu( x )
+         %%% selu activation function %%%
+
+         % init globals
+         ALPHA = 1.6732632423543772848170429916717;
+         LAMBDA = 1.0507009873554804934193349852946;
+         
+         % calculate values
+         negative = LAMBDA * ((ALPHA * exp(x)) - ALPHA);
+         positive = LAMBDA * x;
+         negative (x > 0.0) = 0;
+         positive (x <= 0.0) = 0;
+         % result
+         out = positive + negative;       
+end
